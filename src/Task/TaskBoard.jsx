82a89994977bfc1535c52dfcca1892a28203e09b
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-05-13 20:26:14 +0600

    set data dybamiclay

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBox from './SearchBox';
 import TaskAction from './TaskAction';
+import TaskList from './TaskList';
 
 export default function TaskBoard() {
+    const defaultTask={
+        "id":crypto.randomUUID(),
+        "title":"Learn react",
+        "description":"Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+        "priority":"High",
+        "isFavorite":true,
+        "tags":["web","react","js"]
+    }
+    const [tasks,setTask] = useState([defaultTask])
     return (
         <>
             <section className="mb-20" id="tasks">
@@ -12,74 +22,15 @@ export default function TaskBoard() {
                    <div className="p-2 flex justify-end">
                       <SearchBox/>
                     </div> 
-                   
-                    {/**<!-- Search Box Ends --> */}
+                
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                         {/**your task add */}
                         <TaskAction/>
-                         {/**your task end */}
-                        <div className="overflow-auto">
-                            <table className="table-fixed overflow-auto xl:w-full">
-                                <thead>
-                                    <tr>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-[48px]"></th>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-[300px]"> Title </th>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize w-full"> Description </th>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]"> Tags </th>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]"> Priority </th>
-                                        <th className="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]"> Options </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-                                        <td><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-star" width="24"
-                                            height="24" viewBox="0 0 24 24" stroke-width="2" stroke="yellow" fill="yellow"
-                                            stroke-linecap="round" stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                                        </svg></td>
-                                        <td>Integration API</td>
-                                        <td>
-                                            <div>
-                                                Connect an existing API to a third-party database using
-                                                secure methods and handle data exchange efficiently.
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <ul className="flex justify-center gap-1.5 flex-wrap">
-                                                <li>
-                                                    <span
-                                                        className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">Web</span>
-                                                </li>
-                                                <li>
-                                                    <span
-                                                        className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#1C92FFB0] px-2.5 text-sm capitalize text-[#F4F5F6]">Python</span>
-                                                </li>
-                                                <li>
-                                                    <span
-                                                        className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#FE1A1AB5] px-2.5 text-sm capitalize text-[#F4F5F6]">API</span>
-                                                </li>
-                                            </ul>
-                                        </td>
-                                        <td className="text-center">High</td>
-                                        <td>
-                                            <div className="flex items-center justify-center space-x-3">
-                                                <button className="text-red-500">Delete</button>
-                                                <button className="text-blue-500">Edit</button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                    
+                        <TaskList tasks={tasks}/>
                     </div>
                 </div>
-            </section>
-
+            </section> 
         </>
     );
 };
