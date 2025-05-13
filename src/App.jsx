b71efe207489ca4430e42eec4585298122b0b2e9
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-05-13 18:01:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Hero from "./hero/Hero";
+
+export default function App() {
+  return (
+    <div >
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
+  )
+}
+
+
