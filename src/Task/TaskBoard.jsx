38796bdf3cd87b01449fbf9520467427bc7efbb4
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-05-13 23:39:17 +0600

    add modal

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import SearchBox from './SearchBox';
 import TaskAction from './TaskAction';
 import TaskList from './TaskList';
+import AddTaskModal from './AddTaskModal';
 
 export default function TaskBoard() {
     const defaultTask={
@@ -9,14 +10,18 @@ export default function TaskBoard() {
         "title":"Learn react",
         "description":"Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
         "priority":"High",
-        "isFavorite":true,
+        "isFavorite":false,
         "tags":["web","react","js"]
     }
     const [tasks,setTask] = useState([defaultTask])
+    const [showAddModal,setShowAddModal]=useState(false)
+    // const handleAddTAsk =()=>{
+    //     console.log("Add task")
+    // }
     return (
         <>
             <section className="mb-20" id="tasks">
-
+                {showAddModal && <AddTaskModal/>}
                 <div className="container">
                    {/** <!-- Search Box --> */}
                    <div className="p-2 flex justify-end">
@@ -25,7 +30,7 @@ export default function TaskBoard() {
                 
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
                         {/**your task add */}
-                        <TaskAction/>
+                        <TaskAction onAddClick={()=>setShowAddModal(true)}/>
                     
                         <TaskList tasks={tasks}/>
                     </div>
--- src/Task/TaskList.jsx
+++ src/Task/TaskList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import StarIcon from '../svgIcon/StarIcon';
+import { FaStar } from "react-icons/fa";
+import { CiStar } from 'react-icons/ci';
+const TaskList = ({ tasks }) => {
 
-const TaskList = ({tasks}) => {
-    
     console.log(tasks)
     return (
         <>
@@ -19,39 +20,42 @@ const TaskList = ({tasks}) => {
                         </tr>
                     </thead>
                     <tbody>
-                       {
-                        tasks.map(task=> <tr
-                        key={task.id} className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-                            <td>
-                               <StarIcon/>
-                            </td>
-                            <td>{task.title}</td>
-                            <td>
-                                <div>
-                                    {task.description}
-                                </div>
-                            </td>
-                            <td>
-                                <ul className="flex justify-center gap-1.5 flex-wrap">
-                                    
-                                   {
-                                    task.tags.map(tag=> <li key={tag}>
-                                        <span
-                                            className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">{tag}</span>
-                                    </li>)
-                                   }
-                                    
-                                </ul>
-                            </td>
-                            <td className="text-center">High</td>
-                            <td>
-                                <div className="flex items-center justify-center space-x-3">
-                                    <button className="text-red-500">Delete</button>
-                                    <button className="text-blue-500">Edit</button>
-                                </div>
-                            </td>
-                        </tr>)
-                       }
+                        {
+                            tasks.map(task => <tr
+                                key={task.id} className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
+                                <td>
+                                    <button>
+                                        {task.isFavorite ? <FaStar className='text-yellow-400' /> : <CiStar />}
+                                    </button>
+
+                                </td>
+                                <td>{task.title}</td>
+                                <td>
+                                    <div>
+                                        {task.description}
+                                    </div>
+                                </td>
+                                <td>
+                                    <ul className="flex justify-center gap-1.5 flex-wrap">
+
+                                        {
+                                            task.tags.map(tag => <li key={tag}>
+                                                <span
+                                                    className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">{tag}</span>
+                                            </li>)
+                                        }
+
+                                    </ul>
+                                </td>
+                                <td className="text-center">{task.priority}</td>
+                                <td>
+                                    <div className="flex items-center justify-center space-x-3">
+                                        <button className="text-red-500">Delete</button>
+                                        <button className="text-blue-500">Edit</button>
+                                    </div>
+                                </td>
+                            </tr>)
+                        }
                     </tbody>
                 </table>
             </div>
